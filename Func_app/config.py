522d4fc874fe6d77ff71430a359a8b806ed61296
4ommyx <nattawutth011@gmail.com>
2026-01-20 17:22:28 +0700

    edit config.py for all set50 stock

--- Func_app/config.py
+++ Func_app/config.py
@@ -7,13 +7,9 @@ SET50_TICKERS_BASE = [
     "TOP", "TTB", "TU", "VGI", "WHA", "GLOBAL", "BAM", "CPAXT", "GPSC", "BLA"
 ]
 
-# --- Default Tickers for yfinance (เติม .BK ให้อัตโนมัติ) ---
 SET50_TICKERS = [f"{ticker}.BK" for ticker in SET50_TICKERS_BASE]
 
 # --- Helper Function ---
 def get_tickers(suffix=".BK"):
-    """
-    ดึงรายชื่อหุ้น SET50 พร้อมต่อท้ายนามสกุลที่ต้องการ
-    Example: get_tickers(suffix='') -> ได้ชื่อหุ้นเพียวๆ
-    """
+
     return [f"{ticker}{suffix}" for ticker in SET50_TICKERS_BASE]
